a11a79c1c41a894b3f76c3f242bd6512201a2564
Zhiend <sse_d@qq.com>
2024-04-15 14:31:21 +0800

    学生界面，课表地点和时间加入

--- src/views/Student/selectCourse/querySelectedCourse.vue
+++ src/views/Student/selectCourse/querySelectedCourse.vue
@@ -1,60 +1,31 @@
 <template>
   <div>
     <el-card>
-      <el-table
-          :data="tableData"
-          border
-          style="width: 100%">
-        <el-table-column
-            fixed
-            prop="cid"
-            label="课号"
-            width="150">
+      <el-table :data="tableData" border style="width: 100%">
+        <el-table-column fixed prop="cid" label="课号" width="150">
         </el-table-column>
-        <el-table-column
-            prop="cname"
-            label="课程号"
-            width="150">
+        <el-table-column prop="cname" label="课程号" width="150">
         </el-table-column>
-        <el-table-column
-            prop="tid"
-            label="教师号"
-            width="150">
+        <el-table-column prop="tid" label="教师号" width="150">
         </el-table-column>
-        <el-table-column
-            prop="tname"
-            label="教师名称"
-            width="150">
+        <el-table-column prop="tname" label="教师名称" width="150">
         </el-table-column>
-        <el-table-column
-            prop="ccredit"
-            label="学分"
-            width="150">
+        <el-table-column prop="ccredit" label="学分" width="150">
         </el-table-column>
-        <el-table-column
-            label="操作"
-            width="100">
+        <el-table-column prop="location" label="地点" width="150"></el-table-column>
+        <el-table-column prop="schedule" label="时间" width="150"></el-table-column>
 
+        <el-table-column label="操作" width="100">
           <template slot-scope="scope">
-            <el-popconfirm
-                confirm-button-text='退课'
-                cancel-button-text='取消'
-                icon="el-icon-info"
-                title="确定退课？"
-                @confirm="deleteSCT(scope.row)"
-            >
+            <el-popconfirm confirm-button-text='退课' cancel-button-text='取消' icon="el-icon-info" title="确定退课？"
+              @confirm="deleteSCT(scope.row)">
               <el-button slot="reference" type="text" size="small">退课</el-button>
             </el-popconfirm>
           </template>
         </el-table-column>
       </el-table>
-      <el-pagination
-          background
-          layout="prev, pager, next"
-          :total="total"
-          :page-size="pageSize"
-          @current-change="changePage"
-      >
+      <el-pagination background layout="prev, pager, next" :total="total" :page-size="pageSize"
+        @current-change="changePage">
       </el-pagination>
     </el-card>
   </div>
--- src/views/Student/selectCourse/selectCourseList.vue
+++ src/views/Student/selectCourse/selectCourseList.vue
@@ -1,56 +1,29 @@
 <template>
   <div>
-    <el-table
-        :data="tableData"
-        border
-        show-header
-        stripe
-        style="width: 100%">
-      <el-table-column
-          fixed
-          prop="cid"
-          label="课号"
-          width="150">
+    <el-table :data="tableData" border show-header stripe style="width: 100%">
+      <el-table-column fixed prop="cid" label="课号" width="150">
       </el-table-column>
-      <el-table-column
-          prop="cname"
-          label="课程号"
-          width="150">
+      <el-table-column prop="cname" label="课程号" width="150">
       </el-table-column>
-      <el-table-column
-          prop="tid"
-          label="教师号"
-          width="150">
+      <el-table-column prop="tid" label="教师号" width="150">
       </el-table-column>
-      <el-table-column
-          prop="tname"
-          label="教师名称"
-          width="150">
+      <el-table-column prop="tname" label="教师名称" width="150">
       </el-table-column>
-      <el-table-column
-          label="操作"
-          width="100">
+      <el-table-column prop="location" label="地点" width="150"></el-table-column>
+      <el-table-column prop="schedule" label="时间" width="150"></el-table-column>
+
+      <el-table-column label="操作" width="100">
 
         <template slot-scope="scope">
-          <el-popconfirm
-              confirm-button-text='选择'
-              cancel-button-text='取消'
-              icon="el-icon-info"
-              title="确定选择该教师开设的课程？"
-              @confirm="select(scope.row)"
-          >
+          <el-popconfirm confirm-button-text='选择' cancel-button-text='取消' icon="el-icon-info" title="确定选择该教师开设的课程？"
+            @confirm="select(scope.row)">
             <el-button slot="reference" type="text" size="small">选择</el-button>
           </el-popconfirm>
         </template>
       </el-table-column>
     </el-table>
-    <el-pagination
-        background
-        layout="prev, pager, next"
-        :total="total"
-        :page-size="pageSize"
-        @current-change="changePage"
-    >
+    <el-pagination background layout="prev, pager, next" :total="total" :page-size="pageSize"
+      @current-change="changePage">
     </el-pagination>
   </div>
 </template>
